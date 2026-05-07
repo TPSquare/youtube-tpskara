56786577800210d8d3986ec28da4d6610864a466
TPSquare <t2p0t0p8@gmail.com>
2026-05-08 00:12:07 +0700

    .

--- donate/script.js
+++ donate/script.js
@@ -7,8 +7,9 @@ document.getElementById("notices").innerHTML = languages.notices.map((e) => `<di
 const api = `./notices-verison.json?t=${Date.now()}`;
 const noticesVersion = await fetch(api).then((res) => res.json());
 const currentNoticesVersion = Number(localStorage.getItem("notices-version"));
-if (currentNoticesVersion !== noticesVersion) {
-  document.body.classList.add("show-notices");
+if (currentNoticesVersion !== noticesVersion) document.body.classList.add("show-notices");
+
+document.getElementById("ok-btn").onclick = () => {
+  document.body.classList.remove("show-notices");
   localStorage.setItem("notices-version", noticesVersion);
-}
-document.getElementById("ok-btn").onclick = () => document.body.classList.remove("show-notices");
+};
