01bfd62297f20875435c375f67653df45c88729a
TPSquare <t2p0t0p8@gmail.com>
2026-05-10 12:56:44 +0700

    .

---
+++ videos/script.js
@@ -0,0 +1,13 @@
+const main = document.body.querySelector("main");
+const title = main.querySelector("div");
+window.onresize = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const videoHeight = Math.min(height, (width * 9) / 16);
+  main.style.height = `${videoHeight}px`;
+  title.style.fontSize = `${videoHeight / 25}px`;
+};
+window.onresize();
+
+const headTitle = document.head.querySelector("title");
+headTitle.textContent = title.textContent + headTitle.textContent;
